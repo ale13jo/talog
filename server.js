c8cf84b2cd8da3d2cddb94169576d4135e9645a5
Alejandro Blacut <ale13jo@gmail.com>
2018-04-24 19:57:15 -0600

    v0.1
    mocks

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const app = express()
+
+var db
+
+MongoClient.connect('mongodb://127.0.0.1:27017', (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('talog') // whatever your database name is
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+/*app.listen(3000, function() {
+  console.log('listening on 3000')
+});*/
+app.get('/', function(req, res) {
+  var cursor = db.collection('jobPost').find().toArray(function(err, results) {
+    console.log(results)
+    // send HTML file populated with quotes here
+  })
+  res.sendFile(__dirname + '/index.html')
+});
+
+app.post('/talent', (req, res) => {
+  db.collection('jobPost').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
